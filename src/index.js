86337db48f4c737ab32334c16c65951825ae33a5
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-07 19:43:40 -0600

    Fixed the favicon, it is no longer default

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// When you use bootstrap you can uncomment this
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
